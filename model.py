bc3a54b0127ec42e49daee37943fce4f1a24339d
Chien-Yao Huang <hk850522@gmail.com>
2018-08-30 13:06:47 +0800

    Add Softmax Function in LocationEncoder

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 
 class BaseModel(nn.Module):
@@ -35,6 +36,7 @@ class LocationEncoder(nn.Module):
     def forward(self, data):
         outputs = self.get_hidden_output(data)
         outputs = self.get_spatial_affinity(outputs)
+        return outputs
 
     def get_hidden_output(self, data):
         outputs = []
@@ -46,19 +48,29 @@ class LocationEncoder(nn.Module):
         return outputs
 
     def get_spatial_affinity(self, data):
-        outputs = torch.Tensor([])
+        output = torch.Tensor([])
         for i in range(0, self.pedestrian_num):
             row_data = torch.Tensor([])
             for j in range(0, i+1):
                 row_data = torch.cat([row_data, torch.dot(data[i], data[j]).unsqueeze(0)], dim=0)
-            outputs = torch.cat([outputs, row_data.unsqueeze(0)], dim=0)
+            output = torch.cat([output, row_data.unsqueeze(0)], dim=0)
         '''
         outputs will be like this :
-        a11
-        a21, a22
-        a31, a32, a33
+        <h1, h1>
+        <h2, h1>, <h2, h2>
+        <h3, h1>, <h3, h2>, <h3, h3>
         '''
-        return outputs
+        return self.softmax(output)
+
+    def softmax(self, data):
+        output = torch.zeros(self.pedestrian_num, self.pedestrian_num)
+        for i in range(0, self.pedestrian_num):
+            count = 0
+            for j in range(0, self.pedestrian_num):
+                count += math.exp(data[max(i, j)][min(i, j)].item())
+            for j in range(0, self.pedestrian_num):
+                output[i][j] = math.exp(data[max(i, j)][min(i, j)].item()) / count
+        return output
 
 
 class Encoder(nn.Module):
